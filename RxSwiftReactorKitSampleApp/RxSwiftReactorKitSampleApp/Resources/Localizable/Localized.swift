b79cd258543ae0fdce551d21eec2afe4dd7c09b9
Clint Jang <jang.wangsu@gmail.com>
2018-12-04 09:53:39 +0900

    Feature : Add SwiftGen setting

---
+++ RxSwiftReactorKitSampleApp/RxSwiftReactorKitSampleApp/Resources/Localizable/Localized.swift
@@ -0,0 +1,29 @@
+// swiftlint:disable all
+// Generated using SwiftGen, by O.Halligon — https://github.com/SwiftGen/SwiftGen
+
+import Foundation
+
+// swiftlint:disable superfluous_disable_command
+// swiftlint:disable file_length
+
+// MARK: - Strings
+
+// swiftlint:disable explicit_type_interface function_parameter_count identifier_name line_length
+// swiftlint:disable nesting type_body_length type_name
+internal enum Localized {
+  /// 코딩..
+  internal static let coding = Localized.tr("Localizable", "coding")
+}
+// swiftlint:enable explicit_type_interface function_parameter_count identifier_name line_length
+// swiftlint:enable nesting type_body_length type_name
+
+// MARK: - Implementation Details
+
+extension Localized {
+  private static func tr(_ table: String, _ key: String, _ args: CVarArg...) -> String {
+    let format = NSLocalizedString(key, tableName: table, bundle: Bundle(for: BundleToken.self), comment: "")
+    return String(format: format, locale: Locale.current, arguments: args)
+  }
+}
+
+private final class BundleToken {}
--- RxSwiftReactorKitSampleApp/RxSwiftReactorKitSampleApp/Scenarios/Main/MainViewController.swift
+++ RxSwiftReactorKitSampleApp/RxSwiftReactorKitSampleApp/Scenarios/Main/MainViewController.swift
@@ -9,12 +9,16 @@
 import UIKit
 
 class MainViewController: UIViewController, StoryboardView {
+    @IBOutlet weak var tempLabel: UILabel!
+    
     
     var disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // SwiftGen Test Setting
+        tempLabel.text = Localized.coding
     }
     
     func bind(reactor: MainViewReactor) {
