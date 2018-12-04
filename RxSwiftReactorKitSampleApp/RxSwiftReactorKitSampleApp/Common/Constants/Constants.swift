d2530c3886895adc2335fdf9742651b66d1e30a9
Clint Jang <jang.wangsu@gmail.com>
2018-12-04 10:03:42 +0900

    Feature : Add Constants.swift file

---
+++ RxSwiftReactorKitSampleApp/RxSwiftReactorKitSampleApp/Common/Constants/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  RxSwiftReactorKitSampleApp
+//
+//  Created by Clint on 04/12/2018.
+//  Copyright © 2018 clintjang. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    
+}
