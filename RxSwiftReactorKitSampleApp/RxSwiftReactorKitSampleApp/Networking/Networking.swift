c93d2875d53d5f56c76f78b6793317cae79faac0
Clint Jang <jang.wangsu@gmail.com>
2019-01-06 12:03:14 +0900

    Feature : Add Logger, Networking Function

--- RxSwiftReactorKitSampleApp/RxSwiftReactorKitSampleApp/Networking/Networking.swift
+++ RxSwiftReactorKitSampleApp/RxSwiftReactorKitSampleApp/Networking/Networking.swift
@@ -11,4 +11,42 @@ final class Networking<Target: SugarTargetType>: MoyaSugarProvider<Target> {
         super.init(manager: manager, plugins: plugins)
     }
 
+    func request(
+        _ target: Target,
+        file: StaticString = #file,
+        function: StaticString = #function,
+        line: UInt = #line
+        ) -> Single<Response> {
+        let requestString = "\(target.method) \(target.path)"
+
+        return self.rx.request(target)
+            .filterSuccessfulStatusCodes()
+            .do(
+                onSuccess: { value in
+                    let message = "SUCCESS: \(requestString) (\(value.statusCode))"
+                    log.debug(message, file: file, function: function, line: line)
+                },
+                onError: { error in
+                    if let response = (error as? MoyaError)?.response {
+                        if let jsonObject = try? response.mapJSON(failsOnEmptyData: false) {
+                            let message = "FAILURE: \(requestString) (\(response.statusCode))\n\(jsonObject)"
+                            log.warning(message, file: file, function: function, line: line)
+                        } else if let rawString = String(data: response.data, encoding: .utf8) {
+                            let message = "FAILURE: \(requestString) (\(response.statusCode))\n\(rawString)"
+                            log.warning(message, file: file, function: function, line: line)
+                        } else {
+                            let message = "FAILURE: \(requestString) (\(response.statusCode))"
+                            log.warning(message, file: file, function: function, line: line)
+                        }
+                    } else {
+                        let message = "FAILURE: \(requestString)\n\(error)"
+                        log.warning(message, file: file, function: function, line: line)
+                    }
+                },
+                onSubscribed: {
+                    let message = "REQUEST: \(requestString)"
+                    log.debug(message, file: file, function: function, line: line)
+            }
+        )
+    }
 }
