d6ff4b76f935144f8d36407e27bc9b65fd68dde2
Clint Jang <jang.wangsu@gmail.com>
2019-01-07 18:54:54 +0900

    Fix : Update CounterViewController - reference source : https://github.com/ReactorKit/ReactorKit/tree/master/Examples/Counter - .. will fix it.

--- RxSwiftReactorKitSampleApp/RxSwiftReactorKitSampleApp/Scenarios/Counter/CounterViewController.swift
+++ RxSwiftReactorKitSampleApp/RxSwiftReactorKitSampleApp/Scenarios/Counter/CounterViewController.swift
@@ -11,6 +11,11 @@ import Reusable
 
 class CounterViewController: BaseViewController, StoryboardBased, StoryboardView {
 
+    @IBOutlet var decreaseButton: UIButton!
+    @IBOutlet var increaseButton: UIButton!
+    @IBOutlet var valueLabel: UILabel!
+    @IBOutlet var activityIndicatorView: UIActivityIndicatorView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -27,11 +32,28 @@ class CounterViewController: BaseViewController, StoryboardBased, StoryboardView
 // MARK: Bind
 private extension CounterViewController {
     func bindAction(_ reactor: CounterViewReactor) {
+        increaseButton.rx.tap               // Tap event
+            .map { Reactor.Action.increase }  // Convert to Action.increase
+            .bind(to: reactor.action)         // Bind to reactor.action
+            .disposed(by: disposeBag)
 
+        decreaseButton.rx.tap
+            .map { Reactor.Action.decrease }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
     }
 
     func bindState(_ reactor: CounterViewReactor) {
+        reactor.state.map { $0.value }   // 10
+            .distinctUntilChanged()
+            .map { "\($0)" }               // "10"
+            .bind(to: valueLabel.rx.text)  // Bind to valueLabel
+            .disposed(by: disposeBag)
 
+        reactor.state.map { $0.isLoading }
+            .distinctUntilChanged()
+            .bind(to: activityIndicatorView.rx.isAnimating)
+            .disposed(by: disposeBag)
     }
 }
 
